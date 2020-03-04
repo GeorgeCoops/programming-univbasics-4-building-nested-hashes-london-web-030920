a74f6e56786e770b59a08363d378d489a79f5a6c
George Cooper <coopsgeorge@gmail.com>
2020-03-04 16:54:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
 		:railroads => {
 			:names => {
 				:reading_railroad => {},
-				:key1 => "string1",
+				:pennsylvania_railroad => {},
 				:key2 => "string2",
 				:key3 => "string3"
 			},
