ce0be0c3219fe3edc5f1a2f479f935d35d18ec2f
George Cooper <coopsgeorge@gmail.com>
2020-03-04 16:52:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
 			},
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
-				:key1 => "string1",
+				:two_pieces_owned => 50,
 				:key2 => "string2",
 				:key3 => "string3"
 			}
